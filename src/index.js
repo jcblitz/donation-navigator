84d3e29657b169fc7a9bca3d18327540af41bde6
Jared Blitzstein <jcblitz@users.noreply.github.com>
2018-11-14 09:40:50 -0500

    minor clean up

--- src/index.js
+++ src/index.js
@@ -4,12 +4,5 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Campaign from './pages/Campaign';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<DonationAction />, document.getElementById('donation-action'));
 ReactDOM.render(<Campaign />, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
